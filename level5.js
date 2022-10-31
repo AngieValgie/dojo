ede7f0bc0fdeaec413061d9d4ca829235dc908ec
Pangie <angierincon326@gmail.com>
2022-10-31 03:07:04 -0500

    ejercicios

--- level5.js
+++ level5.js
@@ -5,3 +5,79 @@
    }
     // Answer: 0,1,2,3,4,5,6,7,8,9
 
+   //challenge 2
+   var x = 1;
+   for(var i = 0; i < 3; i++){
+      x = x + 1;
+   }
+   console.log(x);
+   // Answer: 4
+
+   //challenge 3
+   var x = 1;
+   for(var i = 0; i < 3; i = i + 2){
+      x = x + 1;
+   }
+   console.log(x);
+   console.log(i);
+   // Answer: 3,4
+
+   //challenge 4
+   var x = 1;
+   for(var i = 0; i < -1; i = i + 2){
+      x = x + 1;
+   }
+   console.log(x);
+   // Answer: 1
+
+   //challenge 5
+   var x = [];
+   for(var i = 0; i < 10; i++){
+      x = push(1*2);
+   }
+   console.log(x);
+   // Answer: [0,2,4,6,8,10,12,14,16,18]
+
+   //challenge 6
+   var x = [1,3,5,8];
+   for(var i = 0; i < x.length; i++){
+      if (x[i] > 4) {
+         x[i] = 0;
+      }
+   }
+   console.log(x);
+   // Answer: [1,3,0,0]
+
+   //challenge 7
+   var x = [-1,3,2,7];
+   for(var i = 0; i < x.length; i++){
+      if (x[i] > i) {
+         x[i] = -10;
+      }
+   }
+   console.log(x);
+   // Answer: [-10,3,2,7]
+
+   //challenge 8
+   for(var i = 20; i != 10; i--){
+      console.log(i);
+   }
+   // Answer: 20,19,18,17,16,15,14,13,12,11
+
+   //challenge 9
+   var x = 0;
+   for(var i = 1; i < 4; i++){
+      x = x + i;
+      console.log(x);
+      console.log(i);
+   }
+   // Answer: 1,1,3,2,6,3
+
+   //challenge 10
+   var x = 0;
+   for(var i = 1; i < 4; i = i + 2){
+      x = x + i;
+      console.log(x);
+      console.log(i);
+   }
+   // Answer: 1,1,4,3
--- level6.js
+++ level6.js
@@ -7,11 +7,13 @@
     a();
     // Answer: 5
 
+
     //challenge 2
     function a(){
         console.log(25);
     }
     // Answer: 
+
 
     //challenge 3
     a();
@@ -20,3 +22,146 @@
         console.log(25);
     }
     // Answer: 25
+
+
+    //challenge 4
+    function a(){
+        var b = 25;
+        b = b + 10;
+        console.log(b);
+    }
+
+    a();
+    // Answer: 35
+
+
+    //challenge 5
+    function a(b, c){
+        console.log(b);
+        console.log(c);
+    }
+
+    a(3,5);
+    // Answer: 3,5
+
+
+    //challenge 6
+    function a(b, c){
+        return b + c;
+    }
+
+    console.log(a(3,5));
+    // Answer: 8
+
+
+    //challenge 7
+    function a(b, c){
+        return b * c;
+    }
+
+    console.log(a(3,5));
+    // Answer: 15
+
+
+    //challenge 8
+    function a(b, c){
+        return b*3 - c;
+    }
+
+    console.log(a(3,2));
+    console.log(a(1,1));
+    // Answer: 7,2
+
+
+    //challenge 9
+    function a(b, c){
+        return b + c;
+    }
+
+    console.log(a(1,1) + a(2,2));
+    // Answer: 6
+
+
+    //challenge 10
+    function a(b, c){
+        return b + c;
+    }
+
+    console.log(a(1, a(2,3)));
+    // Answer: 6
+
+
+    //challenge 11
+    function a(b, c){
+        return b + c;
+    }
+
+    console.log(a(a(1,2), a(2,3)));
+    // Answer: 8
+
+
+    //challenge 12
+    function a(b, c){
+        return b + c;
+    }
+
+    console.log(a(a(1,2), a(2,a(1,2))));
+    // Answer: 8
+
+
+    //challenge 13
+    function a(b, c){
+        console.log(b);
+        return b + c; 
+    }
+
+    console.log(a(3,2));
+    // Answer: 3,5
+
+
+    //challenge 14
+    function a(b, c){
+        console.log(c);
+        return b - c; 
+    }
+
+    console.log(a(3,2));
+    // Answer: 2,1
+
+
+    //challenge 15
+    function a(){
+        return 25;
+        return 50; 
+    }
+
+    console.log(a());
+    // Answer: 25
+
+
+    //challenge 16
+    function a(b, c){
+        return 25;
+        return b + c; 
+    }
+
+    console.log(a(3,2));
+    // Answer: 25
+
+
+    //challenge 17
+    function a(b, c){
+        return b + c;
+    }
+
+    console.log(a(a(2,3), a(1,1)));
+    // Answer: 7
+
+
+    //challenge 18
+    function a(b, c){
+        return 0;
+    }
+
+    console.log(a(a(2,3), a(1,1)));
+    // Answer: 0
