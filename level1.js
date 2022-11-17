25c883c8a3272a46313078a9b8f6a0d8db2ed9d6
Enyi <91914016+AngieValgie@users.noreply.github.com>
2022-11-17 18:37:02 -0500

    Update level1.js

--- level1.js
+++ level1.js
@@ -2,31 +2,31 @@
         //challenge 1
         var x = 5;
         console.log(x);
-        // Answer: 5
+        // Answer: 
 
         //challenge 2
         var x = 3;
         x = x + 1;
         console.log(x);
-        // Answer: 4
+        // Answer: 
 
         //challenge 3
         var x = 3;
         x = x + 2;
         console.log(x + 2);
         console.log(x);
-        // Answer: 7, 5
+        // Answer: 
 
         //challenge 4
         var x = 3;
         x = x * x;
         console.log(x);
         console.log(x * 2);
-        // Answer: 9, 18
+        // Answer: 
 
         //challenge 5
         var x = 2;
         var y = 5;
         var z = x + y;
         console.log(z);
-        // Answer: 7
+        // Answer: 
